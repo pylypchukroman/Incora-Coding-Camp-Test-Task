2acbc394b9aed67eb13a42868e263b3ee806b878
pylypchukroman <romanpylypchuk@gmail.com>
2022-09-05 21:54:02 +0300

    add home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,23 @@
+import styles from './HomePage.module.scss';
 import Navigation from 'components/Navigation/Navigation';
 
 const HomePage = () => {
   return (
     <>
       <Navigation />
+      <div className={styles.wrapper}>
+        <div className={styles.info}>
+          <p>Welcome to BD Screens</p>
+          <h1 className={styles.title}>
+            Watch Unlimited <br /> Movies, Drama, Music Video <br /> and More
+            Content.
+          </h1>
+          <p>
+            Enjoy popular Movies and Live shows.
+            <br /> Subscribe to your favorite services
+          </p>
+        </div>
+      </div>
     </>
   );
 };
